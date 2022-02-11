23a477c46ed26a183ed9a9436dca1f49f5c17f34
javiercaicedotobon <javiercaicedotobon>
2022-02-10 23:50:58 -0500

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,5 +11,5 @@ printf("Size of an int: %d byte(s)\n", (unsigned long) sizeof(int));
 printf("Size of a long int: %d byte(s)\n", (unsigned long) sizeof(long int));
 printf("Size of a long long int: %d byte(s)\n", (unsigned long) sizeof(long long int));
 printf("Size of a float: %d byte(s)\n", (unsigned long) sizeof(float));
-return(0);
+return (0);
 }
