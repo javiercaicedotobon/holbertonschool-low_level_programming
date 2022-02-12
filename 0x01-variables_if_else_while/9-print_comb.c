95574cfc320311f20b9a01e4bd411390dfc3b054
javiercaicedotobon <javiercaicedotobon@gmail.com>
2022-02-12 12:15:39 -0500

    print_vomb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <studio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - print_comb
+ * Description: print comb
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+for (i = 10; i < 20; i++)
+{
+putchar((i % 10) + '0');
+if (i != 19)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
