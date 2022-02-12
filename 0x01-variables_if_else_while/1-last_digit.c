5efc5a60b4993583c91299c3f7c60e17cf758824
javiercaicedotobon <javiercaicedotobon@gmail.com>
2022-02-11 20:36:49 -0500

    las_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,10 +9,15 @@
  */
 int main(void)
 {
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 if (n > 5)
 printf("%d the string is greater than 5\n", n);
 else if (n == 0)
 printf("%d the string and is 0\n", n);
 else if (n < 6 & n != 0)
 printf("%d the string and is less than 6\n", n);
+return (0);
 }
