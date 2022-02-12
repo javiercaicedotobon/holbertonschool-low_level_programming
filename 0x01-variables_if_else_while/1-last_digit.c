7262a99ddf3e0641556e69c3da50e624a83f06a6
javiercaicedotobon <javiercaicedotobon@gmail.com>
2022-02-11 20:33:11 -0500

    las_digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,18 @@
+#include <studio.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return:always return 0
+ */
+int main(void)
+{
+if (n > 5)
+printf("%d the string is greater than 5\n", n);
+else if (n == 0)
+printf("%d the string and is 0\n", n);
+else if (n < 6 & n != 0)
+printf("%d the string and is less than 6\n", n);
+}
