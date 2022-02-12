ffd5d7f513283ed465aa14e5f5613081a0493c3b
javiercaicedotobon <javiercaicedotobon@gmail.com>
2022-02-11 23:20:39 -0500

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - number
+ *
+ *Retunr: return 0
+ */
+int main(void)
+{
+int i;
+for (i = 0 ; i < 10 ; i++)
+{
+printf("%d", i);
+}
+putchar('\n');
+return (0);
+}
