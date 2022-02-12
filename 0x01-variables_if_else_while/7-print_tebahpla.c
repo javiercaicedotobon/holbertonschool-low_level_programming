203cae2e2cfe027b05f504fe548340bf81fdf1a3
javiercaicedotobon <javiercaicedotobon@gmail.com>
2022-02-11 23:48:56 -0500

    teba

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - tebahpla
+ * Description: print tebahpla
+ * Return: 0
+ */
+
+int main(void)
+{
+char i;
+for (i = 'z' ; i = 'a' ; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return(0);
+}
