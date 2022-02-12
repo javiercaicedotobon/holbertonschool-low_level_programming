e9c26f6680243bb69d34e120aaa25950188547b7
javiercaicedotobon <javiercaicedotobon@gmail.com>
2022-02-11 23:00:46 -0500

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - alphabt
+ *
+ *Return: return 0
+ */
+int main(void)
+{
+char i;
+for (i = 'a' ; i <= 'z' ; i++)
+{
+if (i = 'e' || i = 'q' )
+{
+i = i ;
+}
+putchar (i);
+}
+putchar ('\n');
+return (0);
+}
+
