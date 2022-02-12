8fa03e80169c972b879d748323260913dcb73a34
javiercaicedotobon <javiercaicedotobon@gmail.com>
2022-02-11 21:15:02 -0500

    las_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ if (t >= 5)
 printf("Last digit of %d is %d and is greater than 5\n", n, t);
 else if (t == 0)
 printf("Last digit of %d is %d and is 0\n", n, t);
-else   
+else
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, t);
 return (0);
 }
