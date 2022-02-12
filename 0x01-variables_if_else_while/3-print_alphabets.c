47cd363aae0fae6a33e7b5dbabcbb064525cfa3b
javiercaicedotobon <javiercaicedotobon@gmail.com>
2022-02-11 22:29:29 -0500

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - alphabets
+ *
+ *Return: return 0
+ */
+int main(void)
+{
+char i;
+for (i = 'a' ; i <= 'z' ; i++)
+{
+putchar (i);
+}
+for (i = 'A' ; i <= 'Z' ; i++)
+{
+putchar (i);
+}
+putchar ("\n");
+return (0);
+{
