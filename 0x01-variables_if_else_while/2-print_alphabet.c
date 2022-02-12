4bfd5c0c0460d66ad63e876f114d5b52ccaa144c
javiercaicedotobon <javiercaicedotobon@gmail.com>
2022-02-11 21:44:14 -0500

    print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return:always return 0
+ */
+int main(void)
+{
+int i;
+for (i='a' ;i<='z'; i++){
+putchar[i];
+}
+return (0);
+}
