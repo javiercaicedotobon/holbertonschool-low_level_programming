26e86c54cd9b1e707573a01e6ca405647edb2227
javiercaicedotobon <javiercaicedotobon@gmail.com>
2022-02-16 18:51:08 -0500

    24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,7 +14,12 @@ for (i = 00 ; i <= 24 ; i ++)
 {
 for (j = 00 ; j <= 59 ; j ++)
 {
-_putchar(i,j);
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+_putchar(':');
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
+_putchar('\n');
 }
 }
 }
