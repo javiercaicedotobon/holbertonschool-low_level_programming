1114332f196ba1dd3d1397c04f8de8aad97ce843
javiercaicedotobon <javircaicedotobon@gmail.com>
2022-02-16 11:44:16 -0500

    lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,14 +2,14 @@
 
 /**
  * _islower - cheks for lowercase letrers
- * @a: character to check
+ * @n: character to check
  *
  * Return: 0 or 1
  */
 
 int _islower(int a)
 {
-if (a >= 'a' && a <= 'z')
+if (n >= 'a' && n <= 'z')
 return (1);
 else
 return (0);
