bfbeee909e8698a649f0526df9fe5e8e59b37ac0
javiercaicedotobon <javiercaicedotobon@gmail.com>
2022-02-16 15:05:16 -0500

    last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print las digit of integer input
+ * @n: integer to modify
+ *
+ * Return: 1
+ */
+
+int print_last_digit(int n)
+{
+int m,a;
+a = n % 10;
+if (a < 0)
+a = -a;
+m = '0' + a;
+_putchar(m);
+return (m);
+}
