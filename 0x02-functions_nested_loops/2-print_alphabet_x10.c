bd85dc14f48e8b16dc2325d3a4dd8a2384089af7
javiercaicedotobon <javiercaicedotobon@gmail.com>
2022-02-16 10:27:17 -0500

    alphabet10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10
+ *
+ * Description: print alphabet x10
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+char a;
+int i;
+for (i = 1 ; i = 10 ; i++ )
+{
+for (a = 'a' ; a <= 'z' ; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
+}
